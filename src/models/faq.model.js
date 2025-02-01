a512a9343c46fe389a7c4de511e08a4a32032a73
ShreYansHSachan11 <shreyanshsachan2016@gmail.com>
2025-02-01 16:17:00 +0530

    feat:Update faq model

---
+++ src/models/faq.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const FAQSchema = new mongoose.Schema({
+    
+  question: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 500,
+  },
+
+  answer: {
+    type: String,
+    required: true
+  },
+
+  translations: {
+    type: Map,
+    of: new mongoose.Schema({
+      question: String,
+      answer: String
+    }, { _id: false })
+  },
+
+  language: {
+    type: String,
+    default: 'en',
+    enum: ['en', 'hi', 'bn', 'es', 'fr', 'ja', 'ja', 'ko', 'de', 'it', 'pa'], 
+  },
+ 
+},{ timestamps: true });
+
+FAQSchema.methods.getTranslatedQuestion = function (lang) {
+  return this.translations.get(lang)?.question || this.question;  
+};
+
+FAQSchema.methods.getTranslatedAnswer = function (lang) {
+  return this.translations.get(lang)?.answer || this.answer;  
+};
+
+module.exports = mongoose.model('FAQ', FAQSchema);
